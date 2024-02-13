b6ea87c108a4dddccdc05da35ccb95f7ce16f4ca
DYNOSTAR <joshuamuhorondirangu@gmail.com>
2024-02-13 22:12:40 +0300

    update airbnb

--- models/amenity.py
+++ models/amenity.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-        """Represent an amenity.
-            Attributes:
-                    name (str): The name of the amenity.
-                        """
+    """Represent an amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
 
-                            name = ""
+    name = ""
